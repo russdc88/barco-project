262fcc14d211847b12c55f5966b9a02392f7ebad
russdc88 <russdc88@gmail.com>
2020-01-18 13:03:55 -0700

    first push

---
+++ src/components/Client.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>This is the main page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Client',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
